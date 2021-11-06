3c0b2087b8c893bc84a6901bf666a07afc92a893
Marek Pavlíček <pavlicek.marecek1@seznam.cz>
2021-11-06 16:45:21 +0100

    změna použití storage
    snažím se storageUpload nějak použít
    s onClick na buttonu

--- components/uploadClick.tsx
+++ components/uploadClick.tsx
@@ -1,11 +1,18 @@
+import { useEffect, useState } from "react";
 import storageUpload from "./storage";
-const UploadClick = ({file}) => {
-    const {url} = storageUpload(file);
+const UploadClick = (file) => {
+    const [click, setClick] = useState(false);
+    if(click){
+        storageUpload(file)
+    }   
     return(
-        <div>{url}</div>
+        <>
+        <button onClick={() => setClick(true)}>Upload</button>
+        </>
     )
 }
 
 export default UploadClick
+
 
 
